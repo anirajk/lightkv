941cf2ecc10f6c4390e1cf1f1f628837ef784d02
Aniraj Kesavan <akalathel@zynga.com>
2013-07-06 09:27:30 -0400

    Correcting time calculation

--- utils/benchtool/benchmark.c
+++ utils/benchtool/benchmark.c
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <unistd.h>
+#include <stdint.h>
+#include <inttypes.h>
+// Standard callback
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName){
   int i;
@@ -13,14 +16,22 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName){
   printf("\n");
   return 0;
 }
+uint64_t GetTimeStamp() {
+  struct timeval tv;
+  gettimeofday(&tv,NULL);
+  return tv.tv_sec*(uint64_t)1000000+tv.tv_usec;
+}
+
 
 int main(int argc, char **argv ){
   sqlite3 *db;
   char *zErrMsg = 0, *num_ops  = NULL;
   char cmd[1024], value[] = "value", new_value[] = "new_value";
   int rc, i, c, insert = 0, update = 0, select = 0, ops=0;
-  struct timeval tv;
-  long int ts,tn;
+  uint64_t tcurr, tprev;
+
+
+// Handling options
   while ((c = getopt (argc, argv, "iusn:")) != -1) {
       switch (c) {
           case 'i':
@@ -37,12 +48,18 @@ int main(int argc, char **argv ){
             ops = atoi(num_ops);
             break;
           case '?':
-            if (optopt == 'n')
+            if (optopt == 'n') {
                 fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-            else if (isprint (optopt))
+                printf ("Usage: %c -i<inserts/optional> -u<updates optional> -s<selects optional> -n <number_of_ops/required>",argv[0]);
+            }
+            else if (isprint (optopt)) {
                 fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-            else
+                printf ("Usage: %c -i<inserts/optional> -u<updates optional> -s<selects optional> -n <number_of_ops/required>",argv[0]);
+            }
+            else {
                 fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+                printf ("Usage: %c -i<inserts/optional> -u<updates optional> -s<selects optional> -n <number_of_ops/required>",argv[0]);
+           }
             return 1;
           default:
             abort();
@@ -63,6 +80,8 @@ int main(int argc, char **argv ){
     exit(1);
   }
 
+//defining sql execute
+
   #define RUN(cmd) \
     { \
       rc = sqlite3_exec(db, cmd, callback, 0, &zErrMsg); \
@@ -73,29 +92,27 @@ int main(int argc, char **argv ){
       } \
     }
 
+// Counter
   #define TIME(msg) \
     { \
-      gettimeofday(&tv,NULL); \
-      tn = tv.tv_usec; \
-      printf(msg " : took %ld microsecs\n", (tn - ts)); \
-      ts = tn; \
+      tcurr = GetTimeStamp(); \
+      printf(msg " : took microsecs %"PRIu64" actual \n", (tcurr - tprev)); \
+      tprev = tcurr; \
     }
 
 
-  gettimeofday(&tv, NULL);
-  ts = tv.tv_usec;
+  tprev = GetTimeStamp();
   TIME("'startup'");
-
   RUN("CREATE TABLE kv(k INTEGER,  v VARCHAR(100));");
-  TIME("create table");
+  TIME("table creation");
 
-  RUN("BEGIN;");
-  // 25000 INSERTs in a transaction
+//Insertions
+
   if(insert) {
+  RUN("BEGIN;");
   for (i = 0; i < ops; i++) {
     sprintf(cmd, "INSERT INTO kv VALUES(%d,'%s');",i,value);
     RUN(cmd);
-  }
   }
   printf("%d",ops);
   TIME(" inserts");
@@ -103,11 +120,39 @@ int main(int argc, char **argv ){
   RUN("COMMIT;");
   TIME("commit");
 
-  // Counts
-  RUN("SELECT count(*) FROM kv;");
-  RUN("SELECT count(*) FROM kv WHERE k == 4");
-  RUN("SELECT count(*) FROM kv WHERE v like '%three%';");
-  TIME("selects");
+  }
+
+// Updates
+
+
+  if(update) {
+  RUN("BEGIN;");
+  for (i = 0; i < ops; i++) {
+    sprintf(cmd, "UPDATE kv SET v = '%s' where k=%d;",new_value, i);
+    RUN(cmd);
+  }
+  printf("%d",ops);
+  TIME(" updates");
+
+  RUN("COMMIT;");
+  TIME("commit");
+
+  }
+
+
+//Selects
+
+  if(select) {
+  for (i = 0; i < ops; i++) {
+    sprintf(cmd, "SELECT count(*) FROM kv WHERE k == %d", i);
+    RUN(cmd);
+  }
+  printf("%d",ops);
+  TIME(" reads");
+
+  }
+
+// Counts
 
   sqlite3_close(db);
   return 0;
